cea79da2eb1fdbcff33ab59f03918e0b54ae1eb8
Furqan2710 <161310819+Furqan2710@users.noreply.github.com>
2024-04-11 16:57:47 +0500

    CHANGES COMPLETE

---
+++ main.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { messge: "Enter your first number", type: "number", name: "firstNumber" },
+    { messge: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+// CONDITIONAL STATEMENT
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    ("Please select valid operator");
